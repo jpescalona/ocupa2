2b7d70c8e2f315eebe8a1a9dafd4ed827bf1f120
Juan Pedro Escalona <jescalona@theworkshop.com>
2019-03-30 13:14:02 +0100

    Added install_labels on statup command

--- start.sh
+++ start.sh
@@ -4,3 +4,4 @@ docker-compose build
 docker-compose up -d
 docker-compose exec uwsgi ./manage.py migrate --noinput
 docker-compose exec uwsgi ./manage.py collectstatic --noinput
+docker-compose exec uwsgi ./manage.py install_labels 
